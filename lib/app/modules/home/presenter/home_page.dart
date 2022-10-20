e17f1f77790d277a0d08fb563ba3bb4731be2f70
micaelsn <micaelnascimentocc@gmail.com>
2022-10-19 21:23:28 -0300

    Move home page and store to its own folders

--- lib/app/modules/home/presenter/home_page.dart
+++ lib/app/modules/home/presenter/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
-import 'home_store.dart';
+import '../stores/home_store.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
