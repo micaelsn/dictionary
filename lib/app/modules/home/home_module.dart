0e3b4b8ecd9ea6a328432241eec5e5d828ed1066
micaelsn <micaelnascimentocc@gmail.com>
2022-10-24 19:29:13 -0300

    Add home module depedencies

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_modular/flutter_modular.dart';
+
 import '../favorites/favorites_module.dart';
 import '../history/history_module.dart';
 import '../word_details/word_details_module.dart';
 import '../word_list/word_list_module.dart';
-import 'stores/home_store.dart';
-
+import 'data/external/datasources/word_datasource.dart';
+import 'data/infra/datasource/word_datasource.dart';
+import 'data/infra/repositories/word_repository.dart';
+import 'domain/repositories/word_repository.dart';
+import 'domain/usecases/word_usecase.dart';
 import 'presenter/home_page.dart';
+import 'stores/home_store.dart';
 
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
+    Bind.factory<IWordDatasource>((i) => WordDatasource(i())),
+    Bind.factory<IWordRepository>((i) => WordRepository(i())),
+    Bind.factory<IWordUseCase>((i) => WordUseCase(i())),
     Bind.lazySingleton((i) => HomeStore()),
   ];
 
